449e13d3c9931f099c0f0b1e0f4eaa93fe356724
Hrushikesh Vaidya <hrushikeshrv@gmail.com>
2022-07-16 10:02:44 +0530

    working on new projects view

--- js/project-renderer.js
+++ js/project-renderer.js
@@ -7,13 +7,41 @@ const fetchedJSON = fetch(projectJSON)
     .then(json => {
         for (let project in json) {
             addProjectToNavigation(json[project]);
+            addProjectToNavigation(json[project]);
+            addProjectToNavigation(json[project]);
+            addProjectToNavigation(json[project]);
+            addProjectToNavigation(json[project]);
+            addProjectToNavigation(json[project]);
+            addProjectToNavigation(json[project]);
+            addProjectToNavigation(json[project]);
+            addProjectToNavigation(json[project]);
+            addProjectToNavigation(json[project]);
+            addProjectToNavigation(json[project]);
+            addProjectToNavigation(json[project]);
+            addProjectToNavigation(json[project]);
             renderProject(json[project]);
         }
     });
 
 
 function addProjectToNavigation(project) {
+    const navElement = document.createElement('span');
+    navElement.classList.add('flexbox-row', 'ajc', 'space-lr', 'project-nav-link');
 
+    const projectLogo = document.createElement('img');
+    projectLogo.classList.add('space-lr');
+    projectLogo.src = project.logo.path;
+    projectLogo.width = project.logo.width / 5;
+    projectLogo.height = project.logo.height / 5;
+
+    navElement.appendChild(projectLogo);
+
+    const projectName = document.createElement('span');
+    projectName.classList.add('space-lr');
+    projectName.textContent = project.heading;
+
+    navElement.appendChild(projectName);
+    projectListNavbar.appendChild(navElement);
 }
 
 function renderProject(project) {

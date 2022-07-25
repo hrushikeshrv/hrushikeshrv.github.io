c1c4f29ccd90d252f384b85a45a7ad948f17be14
Hrushikesh Vaidya <hrushikeshrv@gmail.com>
2022-07-25 09:57:58 +0530

    Implemented project renderer

--- js/project-renderer.js
+++ js/project-renderer.js
@@ -9,8 +9,18 @@ fetch(projectJSON)
             addProjectToNavigation(json[project]);
             renderProject(json[project]);
         }
-
         projectListNavbar.firstElementChild.classList.add('current-project');
+    })
+    .then(() => {
+        const outputContainer = document.querySelector('#mjxgui-output-container');
+        const outputTable = document.querySelector('#mjxgui-output');
+        const gui = new MJXGUI('.mjxgui-demo-button', '$$', function() {
+            outputContainer.classList.remove('hidden');
+            let tr = document.createElement('tr');
+            tr.innerHTML = `<td>$$ ${gui.getLatex()} $$</td><td>${gui.getLatex()}</td>`;
+            outputTable.appendChild(tr);
+            MathJax.typesetPromise([tr]).then(() => {})
+        });
     });
 
 
@@ -37,6 +47,44 @@ function addProjectToNavigation(project) {
 }
 
 function renderProject(project) {
-    console.log(project);
-    // TODO - ! Make sure you add support for props like id, classname, and extraHTML
+    let projectDescription = '';
+    for (let desc of project.description) {
+        if (desc instanceof Array) {
+            projectDescription += `<span class="project-desc ${desc.slice(1).join(' ')}">${desc[0]}</span>`;
+        }
+        else {
+            projectDescription += `<span class="project-desc">${desc}</span>`;
+        }
+    }
+
+    let projectLinks = '';
+    for (let link of project.links) {
+        projectLinks += `
+        <span class="flexbox-row ajc link ${link.className ? link.className : ''}" ${link.id ? `id=${link.id}` : ''}>
+            ${link.svg}
+            <span class="space-lr">${link.link}</span>
+        </span>
+        `;
+    }
+
+    let projectLogo = '';
+    if (project.logo) {
+        projectLogo += `<img src="${project.logo.path}" alt="${project.logo.alt}" width="${project.logo.width}" height="${project.logo.height}">`
+    }
+
+    let projectHTML = `
+    <div class="flexbox-row pad-50 mar-20 project">
+        <div class="flexbox-column">
+            ${projectLogo}
+        </div>
+        <div class="flexbox-column project-content">
+            <h2 class="big mbt-10">${project.heading}</h2>
+            ${projectDescription}
+            <span class="flexbox-row pad-10 project-links">${projectLinks}</span>
+            <span class="flexbox-row mt-10 flair">${project.flair}</span>
+        </div>
+        ${project.extraHTML ? project.extraHTML : ''}
+    </div>
+    `;
+    projectContainer.innerHTML += projectHTML;
 }

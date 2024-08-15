0ed44f91975ada2cb3678d400e121e8337caaf54
Hrushikesh Vaidya <hrushikeshrv@gmail.com>
2024-08-15 13:50:47 -0700

    Added contact form

--- js/contact.js
+++ js/contact.js
@@ -15,12 +15,60 @@ const contactLinks = document.querySelectorAll('.contact-link');
 const contactFormContainer = document.querySelector('#contact-form-popup-container');
 const contactFormCloseButton = document.querySelector('#contact-form-popup-container .close-popup-button');
 const submitButton = document.querySelector('#submit');
+const contactAPI = 'https://pzxpy62e3k.execute-api.ap-south-1.amazonaws.com/default/anonymous-share';
+const sendMessageSpinner = document.querySelector('#send-message-spinner');
+const messageSendSuccessMessage = document.querySelector('#message-sent-successfully');
+const messageSendErrorMessage = document.querySelector('#message-send-error');
 
 submitButton.addEventListener('click', (e) => {
     e.preventDefault();
+    messageSendErrorMessage.classList.add('hidden');
+    messageSendSuccessMessage.classList.add('hidden');
+    sendMessageSpinner.classList.add('hidden');
+    contactForm.classList.remove('hidden');
+
+    const messageInput = document.querySelector('#id_message');
+    const nameInput = document.querySelector('#id_name');
+    const emailInput = document.querySelector('#id_email');
+    const subjectInput = document.querySelector('#id_subject');
+    messageInput.value = messageEditor.getData();
     contactForm.reportValidity();
     if (contactForm.checkValidity()) {
-
+        sendMessageSpinner.classList.remove('hidden');
+        contactForm.classList.add('hidden');
+        const requestData = {
+            name: nameInput.value,
+            email: emailInput.value,
+            subject: subjectInput.value,
+            message: messageInput.value,
+        };
+        const request = new Request(
+            contactAPI,
+            {
+                method: 'POST',
+                body: JSON.stringify(requestData),
+            }
+        );
+        fetch(request)
+            .then(response => response.json())
+            .then(response => {
+                console.log(response);
+                nameInput.value = '';
+                emailInput.value = '';
+                subjectInput.value = '';
+                messageEditor.setData('');
+                messageSendSuccessMessage.classList.remove('hidden');
+                messageSendSuccessMessage.scrollIntoView(true);
+            })
+            .catch(error => {
+                console.error(error);
+                messageSendErrorMessage.classList.remove('hidden');
+                messageSendErrorMessage.scrollIntoView(true);
+            })
+            .finally(() => {
+                sendMessageSpinner.classList.add('hidden');
+                contactForm.classList.remove('hidden');
+            })
     }
 })
 
--- lambda_function.py
+++ lambda_function.py
@@ -1,6 +1,55 @@
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
 import json
+import os
+import smtplib, ssl
 
 def lambda_handler(event, context):
-    return {
+    request = json.loads(event['body'])
+    if 'name' in request and 'email' in request and 'message' in request:
+        sent_from = 'hrushikeshspython@gmail.com'
+        gmail_password = os.environ.get('GMAIL_APP_PASSWORD')
+        sent_to = 'hrushikeshrv@gmail.com'
 
+        user_email = request['email']
+        user_name = request['name'] or user_email
+        message_body = request['message']
+        subject = request['subject'] if 'subject' in request else f'Message from {user_name}'
+
+        message = MIMEMultipart('alternative')
+        message['Subject'] = subject or f'Contact From {user_name}'
+        message['From'] = sent_from
+        message['To'] = sent_to
+
+
+        body = f"""
+            <html><head></head><body>
+            <p>
+            <strong>From</strong> - {user_email}<br>
+            <strong>Name</strong> - {user_name}<br>
+            <strong>Subject</strong> - {subject}<br>
+            </p><br>
+
+            {message_body}
+            </body></html>
+        """.strip()
+        message.attach(MIMEText(body, 'html'))
+
+        try:
+            context = ssl.create_default_context()
+            with smtplib.SMTP_SSL('smtp.gmail.com', 465, context=context) as server:
+                server.login(sent_from, gmail_password)
+                server.sendmail(sent_from, sent_to, message.as_string())
+            return {
+                'statusCode': 200,
+                'body': json.dumps(message.as_string())
+            }
+        except Exception as e:
+            return {
+                'statusCode': 500,
+                'body': str(e)
+            }
+    return {
+        'statusCode': 400,
+        'body': '"name", "email", and "message" required in POST body.'
     }
